2f966247ab40e7f00a0dd14e1c325fbcd81fde50
Ikechukwu Anyikwa <austineanyikwa@gmail.com>
2024-08-09 19:50:57 +0100

    set up django rest framework with views to test

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.response import responses
+from rest_framework import status
+from.serializers import MenuSerializer
+from .models import Menu
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
